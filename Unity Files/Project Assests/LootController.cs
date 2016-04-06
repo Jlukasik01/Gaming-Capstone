a4052975f90670020d97736e9fef36278ef561ee
ecellingson <ecellingson@gmail.com>
2016-04-06 15:04:41 -0700

    Added files via upload

---
+++ Unity Files/Project Assests/LootController.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+public class LootController : MonoBehaviour {
+
+    public GameObject[] inventory;
+    public void Start()
+    {
+        int maxSize;
+        maxSize = inventory.Length;
+    }
+    
+    
+    public GameObject dropItem()
+    {
+        GameObject temp;
+        int pick = Random.Range(0, 10);
+        temp = inventory[pick];
+        return temp;
+    }
+}
