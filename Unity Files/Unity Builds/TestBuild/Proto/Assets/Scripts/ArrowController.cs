e7f4b31674aab5d9ecee58e5e1a1af84760f6eb4
stphnhffmnn <stphnhffmnn@gmail.com>
2016-04-16 14:29:32 -0700

    EnemyController
    Kitten SpellCaster Complete, Arrow has physics and deletes itslelf,
    doing damage as well. Gore is updated abit, redid shaders.

--- Unity Files/Unity Builds/TestBuild/Proto/Assets/Scripts/ArrowController.cs
+++ Unity Files/Unity Builds/TestBuild/Proto/Assets/Scripts/ArrowController.cs
@@ -3,25 +3,46 @@ using System.Collections;
 
 public class ArrowController : MonoBehaviour {
     public float speed;
+    public bool isActive;
 	// Use this for initialization
 	void Start () {
-        
+        isActive = true;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-        gameObject.transform.Translate(Vector3.right* Time.deltaTime * speed);
-       // gameObject.transform.Rotate(Vector3.right);
+        if(isActive)
+        {
+            gameObject.transform.Translate(Vector3.right* Time.deltaTime * speed);
+            // gameObject.transform.Rotate(Vector3.right);
+        }
+     
 
-        StartCoroutine("Destory", 5);
+        StartCoroutine("Destroy", 5f);
     }
-    IEnumerator Destroy(float Time) // Attack
+    IEnumerator Destroy(float Time) // Destory object in time
     {
         
         for (float f = 0.0f; f <= Time; f += 0.1f)
         {
-            Destroy(gameObject);
+         
             yield return new WaitForSeconds(0.1f); // wait for animation to be in the position to do damage.
         }
+        Destroy(gameObject);
+    }
+    void OnCollisionEnter(Collision other)
+    {
+        if(other.gameObject.tag == "Object" || other.gameObject.tag == "Enemy")
+        {
+            if (other.gameObject.tag == "Enemy" && isActive)
+            {
+                other.gameObject.GetComponent<EnemyController>().health -= 1;
+            }
+            isActive = false;
+            gameObject.GetComponent<Rigidbody>().useGravity = true;
+            gameObject.GetComponentInChildren<Animator>().speed = 0f;
+            
+        }
+
     }
 }
