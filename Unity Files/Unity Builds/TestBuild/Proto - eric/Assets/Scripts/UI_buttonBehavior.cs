71c5085af63310a35b69e140c9cf23b2b8749d77
ecellingson <ecellingson@gmail.com>
2016-04-24 16:38:25 -0700

    eric -4/24
    stuff

---
+++ Unity Files/Unity Builds/TestBuild/Proto - eric/Assets/Scripts/UI_buttonBehavior.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class UI_buttonBehavior : MonoBehaviour {
+
+    private GameObject player;
+    private GameObject item;
+    private Sprite def;
+    public int invSlot;
+
+
+    // Used to highlight selected item
+    public void intSelect(int k)
+    {
+        if (player.GetComponent<IntController>().inventory[k] != null)
+        {
+            if (k == player.GetComponent<IntController>().keyPress)
+                player.GetComponent<IntController>().changeUIcolor(k, 10);
+            else
+                player.GetComponent<IntController>().changeUIcolor(k, player.GetComponent<IntController>().keyPress);
+            player.GetComponent<IntController>().keyPress = k;
+        }
+    }
+
+    public void switchImage()
+    {
+        if (GetComponent<Image>().enabled)
+        {
+            GetComponent<Image>().enabled = false;
+        }
+        else
+            GetComponent<Image>().enabled = true;
+    }
+
+	// Use this for initialization
+	void Start ()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+        def = GetComponent<Image>().sprite;
+    }
+
+    void Update ()
+    {
+        if (player.GetComponent<IntController>().inventory[invSlot] != null)
+            GetComponent<Image>().sprite = player.GetComponent<IntController>().inventory[invSlot].GetComponent<WeaponController>().ImageUI;
+        
+        else
+        {
+            GetComponent<Image>().sprite = def;
+            player.GetComponent<IntController>().changeUIcolor(invSlot, invSlot);
+        }
+    }
+}
