c73fde0043c110636af11076f0180f679d36bced
stphnhffmnn <stphnhffmnn@gmail.com>
2016-05-03 11:14:31 -0700

    UI Update

---
+++ Unity Files/Unity Builds/TestBuild/Proto/Assets/Scripts/UIButtonControllerInventory.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+public class UIButtonControllerInventory : MonoBehaviour {
+    Image image;
+    public int inventoryNumber;
+    public GameObject player;
+    public GameObject InventoryObject;
+    public Sprite UIImage;
+    public Button invImage;
+    public bool ClickedDown;
+
+    void Start()
+    {
+        image = GetComponent<Image>();
+        invImage = GetComponentInChildren<Button>();
+        player = GameObject.FindGameObjectWithTag("Player");
+    }
+    void Update()
+    {
+        //InventoryObject = player.GetComponent<IntController>().inventory[inventoryNumber];
+        //UIImage = player.GetComponent<IntController>().inventory[inventoryNumber].GetComponent<ItemController>().UIImage;
+        //invImage.image.sprite = UIImage;
+        if (player.GetComponent<IntController>().keyPress == inventoryNumber)
+        {
+            image.color = Color.yellow;
+        }
+        else
+        {
+            image.color = Color.grey;
+
+        }
+    }
+    
+}
--- Unity Files/Unity Builds/TestBuild/Proto/Assets/Scripts/UI_Controller.cs
+++ Unity Files/Unity Builds/TestBuild/Proto/Assets/Scripts/UI_Controller.cs
@@ -9,35 +9,23 @@ public class UI_Controller : MonoBehaviour
     //public Slider manaBarSlider;
     public Text SoulsCounter;
     public Image[] items;
-    public int highlighted;
+    
 
     void Start()
     {
-        highlight(1, 1);
+        
         updateHealth();
     }
 
     void Update()
     {
-        if (highlighted != GetComponent<IntController>().keyPress)
-        {
-            highlight(highlighted, -1);
-            highlight(GetComponent<IntController>().keyPress, 1);
-        }
+     
         healthSlider.maxValue = GetComponent<PlayerController>().maxHealth;
         healthSlider.value = GetComponent<PlayerController>().health;
         SoulsCounter.text = "Souls: " + GetComponent<PlayerController>().souls;
     }
 
-    public void highlight(int a, int b)
-    {
-        if (b == -1)
-            GetComponent<UI_Controller>().items[a].color = Color.gray;
-        else {
-            GetComponent<UI_Controller>().items[a].color = Color.yellow;
-            highlighted = a;
-        }
-    }
+
 
     public void updateHealth()
     {
