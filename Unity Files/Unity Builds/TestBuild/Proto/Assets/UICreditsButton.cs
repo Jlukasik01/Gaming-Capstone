63d4530cd279dc6b9fbd1da9176ccddc08d4f3b4
stphnhffmnn <stphnhffmnn@gmail.com>
2016-05-16 14:41:30 -0700

    UIUpdate

---
+++ Unity Files/Unity Builds/TestBuild/Proto/Assets/UICreditsButton.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+public class UICreditsButton : MonoBehaviour {
+    public Image CreditsImage;
+    // Use this for initialization
+    void Start () {
+	//disable CreditsImage
+    //Add Listeners to enable image on click
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+    // OnClick function
+}
---
+++ Unity Files/Unity Builds/TestBuild/Proto/Assets/UIDirectionsScript.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+public class UIDirectionsScript : MonoBehaviour {
+    public Image DirectionsImage;
+	// Use this for initialization
+	void Start () {
+      // disable Image
+	  // add onclick listener
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    //add activate image
+}
