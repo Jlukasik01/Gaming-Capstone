333eb56596da2e207a8581e913a22c057fa190c2
Jlukasik01 <jlukasik23@gmail.com>
2016-05-18 02:55:09 -0700

    Loot Table Updates

---
+++ Unity Files/Unity Builds/TestBuild/Proto/Assets/Scripts/BlankItem.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+//blank item to be used in loot table. Destroys itself on frame 1
+public class BlankItem : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        Destroy(gameObject);
+	}
+}
--- Unity Files/Unity Builds/TestBuild/Proto/Assets/Scripts/LootController.cs
+++ Unity Files/Unity Builds/TestBuild/Proto/Assets/Scripts/LootController.cs
@@ -4,20 +4,54 @@ using System.Collections;
 
 public class LootController : MonoBehaviour
 {
+    public GameObject[] commonItems;
+    public GameObject[] uncommonItems;
+    public GameObject[] rareItems;
+    public GameObject[] ultraRareItems;
+    public GameObject droppedItem;
+    public GameObject blankItem; 
 
-    public GameObject[] MasterLootTable;
+    public int chanceToDropItem;
+
+
     private int maxSize;
     public void Start()
     {
-         maxSize = MasterLootTable.Length;
+        blankItem = GameObject.FindGameObjectWithTag("blankItem");
     }
 
     //returns random game object from MasterLootTable 
     public GameObject dropItem()
     {
-        GameObject temp;
-        int pick = Random.Range(0, maxSize);
-        temp = MasterLootTable[pick];
-        return temp;
+        if(Random.Range(0, 101) <= chanceToDropItem)
+        {
+            //picks a number between 0 and 100
+            int pick = Random.Range(0, 101);
+            Debug.Log("Picking item " + pick);
+            //50% chance to select common item
+            if (pick <= 50)
+            {
+                droppedItem = commonItems[Random.Range(0, commonItems.Length)];
+            }
+            //30% chance to select uncommon item
+            else if (pick <= 80 && pick > 50)
+            {
+                droppedItem = uncommonItems[Random.Range(0, uncommonItems.Length)];
+            }
+            //15% chance to select rare item
+            else if (pick <= 90 && pick > 75)
+            {
+                droppedItem = rareItems[Random.Range(0, rareItems.Length)];
+            }
+            //5% chance to select ultraRareItem
+            else if (pick <= 100 && pick > 90)
+            {
+                droppedItem = ultraRareItems[Random.Range(0, ultraRareItems.Length)];
+            }
+
+            return droppedItem;
+        }
+        return blankItem;
+        
     }
 }
